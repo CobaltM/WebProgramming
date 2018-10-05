12717c9cc97f3ca173074f16b5f0e7c0a820fb5c
scaldwell9 <ethan112096@gmail.com>
2018-10-04 21:14:37 -0400

    modified:   a1/public_html/index.html 	modified:   inclass4/formtest.html 	new file:   inclass4/formtest.php 	modified:   inclass4/test.html

---
+++ inclass4/formtest.php
@@ -0,0 +1,6 @@
+<?php
+echo "1> YOUR NAME IS ".$_POST["visitor_name"]."<br>";
+echo "2> YOUR SELECTED ".$_POST["account_type"]." ACCOUNT<br>";
+echo "3> YOUR OS IS ".$_POST["system"]."<br>";
+echo "4> YOUROPTIONAL FEATURES ARE ".$_POST["options"]."<br>";
+?>
